a050e2b662f70aa24bd91111d7488d4fa9f7d81c
Dmitriy Volchenko <dmitriywolf@users.noreply.github.com>
2020-05-13 21:27:21 +0300

    add modals

--- app/js/app.js
+++ app/js/app.js
@@ -28,9 +28,50 @@ document.addEventListener('DOMContentLoaded', () => {
     navHeader();
 
 
-
-
     /*Modals*/
+    const modals = () => {
+        function bindModal(triggerSelector, modalSelector, closeSelector) {
+
+            const trigger = document.querySelectorAll(triggerSelector),
+                modal = document.querySelector(modalSelector),
+                close = document.querySelector(closeSelector);
+
+            trigger.forEach(item => {
+                item.addEventListener('click', (e) => {
+                    if (e.target) {
+                        e.preventDefault();
+                    }
+                    modal.classList.add('show', 'animated', 'bounceIn');
+                    document.body.classList.add('show-popup');
+
+                });
+
+            });
+
+            close.addEventListener('click', () => {
+                modal.classList.remove('show');
+                document.body.classList.remove('show-popup');
+            });
+
+            modal.addEventListener('click', (e) => {
+                if (e.target === modal) {
+                    modal.classList.remove('show');
+                    document.body.classList.remove('show-popup');
+                }
+            })
+
+        }
+
+        function showModalBuTime(selector, time) {
+            setTimeout(function () {
+                document.querySelector(selector).style.display = 'block';
+            }, time);
+        }
+
+        bindModal('.button--callback', '.popup--callback', '.popup--callback .popup__close');
+    };
+
+    modals();
 
 
 });
