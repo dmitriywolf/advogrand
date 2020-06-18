a32a02542e6212b9cd7a71a8c69340a7914d0273
Dmitriy Volchenko <dmitriywolf@users.noreply.github.com>
2020-06-18 14:11:59 +0300

    add navigation

--- src/js/index.js
+++ src/js/index.js
@@ -4,9 +4,11 @@ document.addEventListener('DOMContentLoaded', () => {
     const navHeader = () => {
         let headerTop = document.querySelector('.header__top');
         let intro = document.getElementById('intro');
-
+        let burger = document.querySelector('.burger');
+        let nav = document.querySelector('.nav__list ');
         let introHeight = intro.offsetHeight;
         let scrollPosition = window.pageYOffset || document.documentElement.scrollTop;
+
 
         //Fixed TopHeader
         window.addEventListener('scroll', () => {
@@ -20,7 +22,23 @@ document.addEventListener('DOMContentLoaded', () => {
             }
 
         });
+        burger.addEventListener('click', function () {
+            if (!this.classList.contains('burger--close')) {
+                this.classList.add('burger--close');
+                nav.classList.add('show', 'bounceInLeft');
+            } else {
+                this.classList.remove('burger--close');
+                nav.classList.remove('show', 'bounceInLeft');
+            }
+        });
 
+        nav.addEventListener('click', function (event) {
+            let element = event.target;
+            if (element.classList.contains('nav__link') && nav.classList.contains('show')) {
+                nav.classList.remove('show');
+                burger.classList.remove('burger--close');
+            }
+        });
 
     };
     navHeader();
